e65c81500a5ee83118cb406ad5765376dc599944
fbiwenjingyu <fbiwenjingyu@163.com>
2018-07-31 18:55:34 +0800

    cc

--- src/Result.java
+++ src/Result.java
@@ -1,6 +1,6 @@
 
 public enum Result {
-	success(1,"sucess"),failure(2,"failure");
+	success(1,"sucess"),failure(2,"failure"),nameExist(3,"nameAlreadyExists");
 	private int code;
 	private String msg;
 	Result(int code,String msg){
